84e91cf480101b8e1b33ee062b4b7e19074c481a
Lucas Alves <lucasalvesf78@gmail.com>
2021-08-27 22:54:25 -0300

    Roteiro 6 - teste com mais dados

---
+++ Roteiro 6/meu_grafo_matriz_adjacencia_dir.py
@@ -0,0 +1,30 @@
+from bibgrafo.grafo_matriz_adj_dir import GrafoMatrizAdjacenciaDirecionado
+from bibgrafo.grafo_exceptions import *
+from copy import deepcopy
+
+class MeuGrafo(GrafoMatrizAdjacenciaDirecionado):
+
+    def __warshallmatrix(self):
+        E = []
+        tam = len(self.N)
+        for i in range(tam):
+            M = []
+            for j in range(tam):
+                M.append(0)
+            E.append(M)
+        for i in range(tam):
+            for j in range(tam):
+                if len(self.M[i][j]) > 0 and self.M[i][j] != '-':
+                    E[i][j] = 1
+        return E
+        
+    def warshall(self):
+        matriz = self.__warshallmatrix()
+        tam=len(self.N)
+
+        for i in range(tam):
+            for j in range(tam):
+                if(matriz[j][i] == 1):
+                    for k in range(tam):
+                        matriz[j][k] = max(matriz[j][k],matriz[i][k])
+        return matriz
