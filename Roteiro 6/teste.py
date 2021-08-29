1f3cd524c7ff10310b803ac13bc7a50be6923065
Lucas Alves <lucasalvesf78@gmail.com>
2021-08-29 19:09:40 -0300

    atualizaçao das arestas paralelas

--- Roteiro 6/teste.py
+++ Roteiro 6/teste.py
@@ -3,17 +3,16 @@ from bibgrafo.grafo_lista_adjacencia import GrafoListaAdjacencia
 
 from meu_grafo_matriz_adjacencia_dir import MeuGrafo
 
-paraiba = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+paraiba = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T'])
 
 paraiba.adicionaAresta("a1", "J", "C")
 paraiba.adicionaAresta("a2", "C", "E")
-paraiba.adicionaAresta("a3", "C", "E")
+paraiba.adicionaAresta("a3", "E", "C")
 paraiba.adicionaAresta("a4", "C", "P")
-paraiba.adicionaAresta("a5", "C", "P")
+paraiba.adicionaAresta("a5", "P", "C")
 paraiba.adicionaAresta("a6", "C", "M")
 paraiba.adicionaAresta("a7", "C", "T")
 paraiba.adicionaAresta("a8", "M", "T")
-paraiba.adicionaAresta("a9", "T", "Z")
 
 print(paraiba)
 print(paraiba.warshall())
